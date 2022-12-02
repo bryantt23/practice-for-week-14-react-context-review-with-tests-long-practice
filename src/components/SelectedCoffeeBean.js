df5d336e6ac93ddc91e6abb4d5c8347bb2e2a294
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-12-02 01:21:25 -0800

    pass first tests

--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -1,5 +1,18 @@
+import { CoffeeContext } from '../context/CoffeeContext';
+import { createContext, useContext, useState } from 'react';
+const useCoffee = () => useContext(CoffeeContext);
+
 const SelectedCoffeeBean = () => {
-  return <div className='selected-coffee'></div>;
+  const { coffeeBean } = useCoffee();
+  console.log(
+    '🚀 ~ file: SelectedCoffeeBean.js:7 ~ SelectedCoffeeBean ~ coffeeBean',
+    coffeeBean
+  );
+  return (
+    <div className='selected-coffee'>
+      <h2>{coffeeBean.name}</h2>
+    </div>
+  );
 };
 
 export default SelectedCoffeeBean;
